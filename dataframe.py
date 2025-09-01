8432ee1f327b3fe292938d39835e0f375ceb3e82
Marcello Cavalcanti de Albuquerque <marcello.albuquerque@larcopetroleo.com.br>
2025-09-01 16:34:33 -0300

    example dataframe

---
+++ dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+base = {
+        'Nomes':['Ana', 'Carlos', 'Gabriela', 'Maria'],
+        'Fones':[9988774411, 9988774455, 9988774466, 9988778822]
+        }
+
+data = pd.DataFrame(base)
+
+print(data)
+
